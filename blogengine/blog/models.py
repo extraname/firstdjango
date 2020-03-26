0ed66ea351d47bf2bdd42416d4292fa3ad4a4454
extraname <zorraider@gmail.com>
2020-03-26 17:26:37 +0200

    end of youtubetraining

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 from django.shortcuts import reverse
 
+from django.utils.text import slugify
+from time import time
+
+
+def gen_slug(s):
+    new_slug = slugify(s, allow_unicode=True)
+    return new_slug + '-' + str(int(time()))
+
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
     body = models.TextField(blank=True, db_index=True)
     tags = models.ManyToManyField("Tag", blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
@@ -14,8 +22,23 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("post_detail_url", kwargs={'slug': self.slug})
 
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
+
+    def get_update_url(self):
+        return reverse('post_update_url', kwargs={'slug': self.slug})
+
+    def get_delete_url(self):
+        return reverse('post_delete_url', kwargs={'slug': self.slug})
+
     def __str__(self):
-        return f'{self.title}'
+        return self.title
+
+
+    class Meta:
+        ordering = ['-date_pub']
 
 
 class Tag(models.Model):
@@ -23,7 +46,16 @@ class Tag(models.Model):
     slug = models.SlugField(max_length=50, unique=True)
 
     def __str__(self):
-        return f"{self.title}"
+        return self.title
 
     def get_absolute_url(self):
         return reverse("tag_detail_url", kwargs={'slug': self.slug})
+
+    def get_update_url(self):
+        return reverse('tag_update_url', kwargs={'slug': self.slug})
+
+    def get_delete_url(self):
+        return reverse('tag_delete_url', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['title']
